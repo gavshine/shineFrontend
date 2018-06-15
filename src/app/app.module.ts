8d0e495685151f89c7768d99cc01e79666aa2266
Gaveen <gavneliah@gmail.com>
2018-06-15 14:52:55 +0400

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ParticlesModule } from 'angular-particle';
+import { RouterModule, Routes } from '@angular/router';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { NavComponent } from './layout/nav/nav.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { ServicesComponent } from './pages/services/services.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactUsComponent },
+  { path: 'services', component: ServicesComponent },
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    FooterComponent,
+    NavComponent,
+    ContactUsComponent,
+    ServicesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ParticlesModule,
+   RouterModule.forRoot(
+      appRoutes
+    ),
+   AngularFontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/layout/nav/nav.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  toggle;
+  nav;
+  toggle_open_text = 'Menu';
+  toggle_close_text = 'Close'; 
+  active = false;  
+    
+  constructor() { }
+
+  ngOnInit() {
+    this.toggle = document.querySelectorAll(".toggle")[0];
+    this.nav = document.querySelectorAll("nav")[0];
+    this.toggle.innerHTML = this.toggle_open_text;
+  }
+    
+   openMenu(active){
+       console.log(active);
+       //console.log(this.nav);
+       //alert(JSON.stringify(event.target.classList));
+       this.nav.classList.toggle('open');
+
+      if (active) {
+        this.active = false;
+        this.toggle.innerHTML = this.toggle_open_text;
+      } else {
+        this.active = true;
+        this.toggle.innerHTML = this.toggle_close_text;
+      }
+   }
+}
